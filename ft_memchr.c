d15756af768cfdda3b8b5ff6b83db77fb6074088
Lakrim Ossama <ossama.lakrim@uit.ac.ma>
2024-10-28 20:26:19 +0100

    Implement ft_memchr for finding bytes in memory

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osslakri <osslakri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:43:05 by osslakri          #+#    #+#             */
+/*   Updated: 2023/12/12 21:00:28 by osslakri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s + i) == (unsigned char)c)
+		{
+			return ((void *)s + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
