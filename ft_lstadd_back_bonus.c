321a72c71095dc7222f99cdd638b5d708971f57e
Lakrim Ossama <ossama.lakrim@uit.ac.ma>
2024-10-28 20:21:13 +0100

    Add ft_lstadd_back_bonus to append to the list

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osslakri <osslakri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 00:35:59 by osslakri          #+#    #+#             */
+/*   Updated: 2023/12/02 12:09:23 by osslakri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*ptr;
+
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	ptr = *lst;
+	while (ptr -> next)
+	{
+		ptr = ptr -> next;
+	}
+	ptr->next = new;
+}
