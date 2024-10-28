544365008df7674283947ec7e39491c7bdb78649
Lakrim Ossama <ossama.lakrim@uit.ac.ma>
2024-10-28 20:21:53 +0100

    Implement ft_lstadd_front_bonus to prepend to the list

---
+++ ft_lstadd_front_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osslakri <osslakri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 23:23:06 by osslakri          #+#    #+#             */
+/*   Updated: 2023/12/05 12:10:17 by osslakri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst)
+		return ;
+	else if (!new)
+		return ;
+	new->next = *lst ;
+	*lst = new ;
+}
