25120be14e361379a8264a2e5b648897e5b4db36
Lakrim Ossama <ossama.lakrim@uit.ac.ma>
2024-10-28 20:25:11 +0100

    Implement ft_lstnew_bonus to create new list node

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osslakri <osslakri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 05:48:38 by osslakri          #+#    #+#             */
+/*   Updated: 2023/12/05 12:10:48 by osslakri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (new);
+	(*new).content = content;
+	(*new).next = NULL;
+	return (new);
+}
